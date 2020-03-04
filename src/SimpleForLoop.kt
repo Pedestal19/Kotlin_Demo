537ff79119bec807da3e2befa6385b6003639929
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-04 23:19:59 +0100

    SimpleForLoop

---
+++ src/SimpleForLoop.kt
@@ -0,0 +1,14 @@
+/**
+ * Author: Hosanna Gabe-Oji.
+ *Project: Kotlin_Demo.
+ *Date: 3/4/2020.
+ */
+fun main(args:Array<String>){
+
+    for(item in 1..10){
+        if(item==2)
+        println("count: $item")
+    }
+
+    println("loop done")
+}
