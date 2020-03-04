1e5ee70782336cd9a7d63c0043780e53c1d47da8
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-04 18:24:13 +0100

    LogicCondition

---
+++ src/LogicCondition.kt
@@ -0,0 +1,16 @@
+/**
+ * Author: Hosanna Gabe-Oji.
+ *Project: Kotlin_Demo.
+ *Date: 3/4/2020.
+ */
+
+fun main(args:Array<String>){
+    var n:Int=9
+    println(n>=0 && n<10)
+
+    var n2:Int=55
+    println(n2==10 || n<100)
+
+    var IsMarried:Boolean=true
+    print(!IsMarried)
+}
