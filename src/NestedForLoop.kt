9532ad80ec04852294b16e9dfefe97caae04d1f5
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-04 23:24:16 +0100

    NestedForLoop

---
+++ src/NestedForLoop.kt
@@ -0,0 +1,22 @@
+/**
+ * Author: Hosanna Gabe-Oji.
+ *Project: Kotlin_Demo.
+ *Date: 3/4/2020.
+ */
+
+fun main(args:Array<String>){
+
+    for(count1 in 1..5){
+
+        println("count1:$count1")
+
+        for(count2 in 1..7){
+            println("count2: $count2")
+        }
+
+        println("loop nested done")
+
+    }
+
+    println("loop done")
+}
