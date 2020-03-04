ebf23de88c28e5ff4c54094144f72fa6d9f84c7a
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-04 15:58:00 +0100

    Null safety

---
+++ src/NullSafety.kt
@@ -0,0 +1,18 @@
+/**
+ * Author: Hosanna Gabe-Oji.
+ *Project: Kotlin_Demo.
+ *Date: 3/4/2020.
+ */
+
+fun main(args:Array<String>){
+
+//    var name: String
+//    name=null cannot assign null value
+
+    var name: String?
+    name=null
+    print(name!!)
+
+    //? meaans var can accept null values
+    //!! means this value has to have a value otherwise throw exceptions
+}
