f8b868a98ed411bde1eabad9ecaed0942db88d4a
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-04 23:34:55 +0100

    WhileLoop

---
+++ src/WhileLoop.kt
@@ -0,0 +1,19 @@
+/**
+ * Author: Hosanna Gabe-Oji.
+ *Project: Kotlin_Demo.
+ *Date: 3/4/2020.
+ */
+
+fun main(args:Array<String>){
+    var count =1
+    while(count <= 5){
+
+        println("count: $count")
+
+        if(count%2==0){
+            println("Number is even: $count")
+        }
+
+        count++
+    }
+}
