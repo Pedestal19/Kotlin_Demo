ce7312e73d32bdd37c2158495034111176304553
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-08 20:32:28 +0100

    Set

---
+++ src/Sets.kt
@@ -0,0 +1,15 @@
+/**
+ * Author: Hosanna Gabe-Oji.
+ *Project: Kotlin_Demo.
+ *Date: 3/8/2020.
+  */
+
+fun main (args:Array<String>){
+
+    var setElement = setOf(1,2,3,1,44,55,55)
+
+    for(element in setElement){
+        println(element)
+    }
+
+}
